7f9a67847270c35cabd7c5b3c6a9d60e343481b4
S. M <sbug1442@gmail.com>
2021-11-16 08:27:18 -0500

    Initial Commit

---
+++ headers.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+struct symbols {
+	int	DefinedOnSourceLine;
+	int	Address;
+	char	Name[7]; };
+*/
+
+#ifndef HEADERS
+#define HEADERS
+struct symbol{
+	int address;
+	int sourceLine;
+	char name[7];
+};
+struct syminst{
+    char iname[5];
+    int opcode;
+};
+struct countertrack{
+    int line;
+    unsigned int counter;
+};
+int matchDirective(char* name);
+int checkspecial(char* symbol);
+int symbolExists(struct symbol* tab[], char *sname);
+int IsAValidSymbol( char *TestSymbol, struct symbol* tab[] );
+#endif
